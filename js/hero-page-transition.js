48af0a6df249283135fe27d428e1272eaad5f516
Lightmonster224 <jinzhaoteh@gmail.com>
2024-08-08 12:06:09 +1200

    Updaye

--- js/hero-page-transition.js
+++ js/hero-page-transition.js
@@ -2,32 +2,53 @@ document.addEventListener("DOMContentLoaded", () => {
   const slider = document.getElementById("slider");
   const slides = document.querySelectorAll(".slide");
   let currentIndex = 0;
+  let currentIndexY = 0;
 
   // Update the slider position
   function updateSliderPosition(type) {
-
-    slider.style.transform = `translateX(-${currentIndex * 100}vw)`;
+    console.log(type)
+    if (type == "x") {
+      slider.style.transform = `translateX(-${currentIndex * 100}vw)`;
+    } else if (type == "y") {
+      slider.style.transform = `translateY(-${currentIndexY * 100}vw)`;
+    }
   }
 
-  // Show the next slide
+  // Show the next slide, x
   function nextSlide() {
     if (currentIndex < slides.length - 1) {
       currentIndex++;
-      updateSliderPosition();
+      updateSliderPosition("x");
     }
   }
 
-  // Show the previous slide
+  // Show the previous slide, x
   function prevSlide() {
     if (currentIndex > 0) {
       currentIndex--;
-      updateSliderPosition();
+      updateSliderPosition("x");
     }
   }
-
+  // Show the next slide, y
+  function nextSlideY() {
+    if (currentIndex < slides.length - 1) {
+      currentIndexY++;
+      if (currentIndexY > 0) {
+        Array.prototype.slice.call(document.getElementsByClassName("Hero-bttn"), 0).forEach(element => {
+          element.style.display = "None";
+        });
+      } else {
+        Array.prototype.slice.call(document.getElementsByClassName("Hero-bttn"), 0).forEach(element => {
+          element.style.display = "block";
+        });
+      }
+      updateSliderPosition("y");
+    }
+  };
   // Attach event listeners to the buttons
   document.getElementById("next").addEventListener("click", nextSlide);
   document.getElementById("prev").addEventListener("click", prevSlide);
+  document.getElementById("Hero-text1").addEventListener("click", nextSlideY);
 
   // Initial position setup
   updateSliderPosition();
--- js/links.js
+++
@@ -1,8 +0,0 @@
-let HeroText1 = document.getElementById("Hero-text1") 
-HeroText1.addEventListener("click", function() {
-  //slide window up
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
